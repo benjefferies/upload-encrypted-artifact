34a7aedbea77f0dc0ba3c6e4edadd143b299937a
Ben Jefferies <benjefferies@echosoft.uk>
2023-11-29 10:14:41 +0000

    Use Data Key to encrypt larger files

--- src/encrypt.ts
+++ src/encrypt.ts
@@ -1,26 +1,39 @@
-import {
-  CommitmentPolicy,
-  KmsKeyringNode,
-  buildEncrypt
-} from '@aws-crypto/client-node'
+import {GenerateDataKeyCommand, KMSClient} from '@aws-sdk/client-kms'
 import {readFileSync, writeFileSync} from 'fs'
+
+import crypto from 'crypto'
+
+const client = new KMSClient()
 
 export async function encryptFile(
   filePath: string,
   kmsKeyId: string
 ): Promise<void> {
-  const keyring = new KmsKeyringNode({generatorKeyId: kmsKeyId})
-
-  const client = buildEncrypt(CommitmentPolicy.REQUIRE_ENCRYPT_ALLOW_DECRYPT)
   // Read the file content
   const fileBuffer = readFileSync(filePath)
 
   try {
     // Encrypt the data
-    const {result} = await client.encrypt(keyring, fileBuffer)
+    const command = new GenerateDataKeyCommand({
+      KeyId: kmsKeyId
+    })
+    const {CiphertextBlob, Plaintext} = await client.send(command)
+
+    if (!Plaintext || !CiphertextBlob) {
+      throw new Error('No encryption key returned from KMS')
+    }
+    // Use the plaintext encryption key to encrypt the file
+    const cipher = crypto.createCipheriv(
+      'aes-256-cbc',
+      Plaintext,
+      crypto.randomBytes(16)
+    )
+    let encrypted = cipher.update(fileBuffer)
+    encrypted = Buffer.concat([encrypted, cipher.final()])
 
     // Overwrite file with encrypted data
-    writeFileSync(filePath, result)
+    writeFileSync(filePath, encrypted)
+    writeFileSync(`${filePath}.key`, CiphertextBlob)
     console.log('File encrypted successfully')
   } catch (error) {
     console.error('Error encrypting file:', error)
